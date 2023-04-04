41d06ee07d8c12670e381e2ca246e36fd196829d
khanhhg27 <128038710+khanhhg27@users.noreply.github.com>
2023-04-04 21:34:12 +0700

    Update test.cpp

--- DrawandDelete/test.cpp
+++ DrawandDelete/test.cpp
@@ -1,4 +1,4 @@
-void GamePlay::drawI(Cell** pBoard, Cell cell_1, Cell cell_2) {
+void Board::drawI(Cell cell_1, Cell cell_2) {
 	Console::setColor(WHITE, RED);
 	Cell tmp_1 = cell_1;
 	Cell tmp_2 = cell_2;
@@ -7,57 +7,31 @@ void GamePlay::drawI(Cell** pBoard, Cell cell_1, Cell cell_2) {
 	int x2 = (tmp_2.x_console * 10) + 5;
 	int y2 = (tmp_2.y_console * 4) + 2;
 	if (x1 == x2) {
-		if (y1 < y2){
-			Console::gotoXY(x1, y1 + 1);
-			putchar(30);
-			for (int i = y1 + 2; i <= y2 - 2; i++) {
-				Console::gotoXY(x1, i);
-				putchar(179);
-			}
-			Console::gotoXY(x2, y2 - 1);
-			putchar(31);
-			return;
+		Console::gotoXY(x1, y1 + 1);
+		putchar(30);
+		for (int i = y1 + 2; i <= y2 - 2; i++) {
+			Console::gotoXY(x1, i);
+			putchar(179);
 		}
-		else{
-			Console::gotoXY(x1, y1 - 1);
-			putchar(31);
-			for (int i = y1 - 2; i >= y2 + 2; i--) {
-				Console::gotoXY(x1, i);
-				putchar(179);
-			}
-			Console::gotoXY(x2, y2 + 1);
-			putchar(30);
-			return;
-		}
+		Console::gotoXY(x2, y2 - 1);
+		putchar(31);
+		return;
 	}
 	//========================================================================//
 	if (y1 == y2) {
-		if (x1 < x2){
-			Console::gotoXY(x1 + 1, y1);
-			putchar(17);
-			for (int i = x1 + 2; i <= x2 - 2; i++) {
-				Console::gotoXY(i, y1);
-				putchar(45);
-			}
-			Console::gotoXY(x2 - 1, y2);
-			putchar(16);
-			return;
+		Console::gotoXY(x1 + 1, y1);
+		putchar(17);
+		for (int i = x1 + 2; i <= x2 - 2; i++) {
+			Console::gotoXY(i, y1);
+			putchar(45);
 		}
-		else{
-			Console::gotoXY(x1 - 1, y1);
-			putchar(16);
-			for (int i = x1 - 2; i >= x2 + 2; i--) {
-				Console::gotoXY(i, y1);
-				putchar(45);
-			}
-			Console::gotoXY(x2 + 1, y2);
-			putchar(17);
-			return;
-		}
+		Console::gotoXY(x2 - 1, y2);
+		putchar(16);
+		return;
 	}
 }
 
-void GamePlay::deleteI(Cell** pBoard, Cell cell_1, Cell cell_2) {
+void Board::deleteI(Cell cell_1, Cell cell_2) {
 	Console::setColor(WHITE, WHITE);
 	Cell tmp_1 = cell_1;
 	Cell tmp_2 = cell_2;
@@ -66,68 +40,39 @@ void GamePlay::deleteI(Cell** pBoard, Cell cell_1, Cell cell_2) {
 	int x2 = (tmp_2.x_console * 10) + 5;
 	int y2 = (tmp_2.y_console * 4) + 2;
 	if (x1 == x2) {
-		if (y1 < y2){
-			Console::gotoXY(x1, y1 + 1);
+		Console::gotoXY(x1, y1 + 1);
+		putchar(32);
+		for (int i = y1 + 2; i <= y2 - 2; i++) {
+			Console::gotoXY(x1, i);
 			putchar(32);
-			for (int i = y1 + 2; i <= y2 - 2; i++) {
-				Console::gotoXY(x1, i);
-				putchar(32);
-			}
-			Console::gotoXY(x2, y2 - 1);
-			putchar(32);
-			return;
 		}
-		else{
-			Console::gotoXY(x1, y1 - 1);
-			putchar(32);
-			for (int i = y1 - 2; i >= y2 + 2; i--) {
-				Console::gotoXY(x1, i);
-				putchar(32);
-			}
-			Console::gotoXY(x2, y2 + 1);
-			putchar(32);
-			return;
-		}
+		Console::gotoXY(x2, y2 - 1);
+		putchar(32);
+		return;
 	}
 	//========================================================================//
 	if (y1 == y2) {
-		if (x1 < x2){
-			Console::gotoXY(x1 + 1, y1);
+		Console::gotoXY(x1 + 1, y1);
+		putchar(32);
+		for (int i = x1 + 2; i <= x2 - 2; i++) {
+			Console::gotoXY(i, y1);
 			putchar(32);
-			for (int i = x1 + 2; i <= x2 - 2; i++) {
-				Console::gotoXY(i, y1);
-				putchar(32);
-			}
-			Console::gotoXY(x2 - 1, y2);
-			putchar(32);
-			return;
 		}
-		else{
-			Console::gotoXY(x1 - 1, y1);
-			putchar(32);
-			for (int i = x1 - 2; i >= x2 + 2; i--) {
-				Console::gotoXY(i, y1);
-				putchar(32);
-			}
-			Console::gotoXY(x2 + 1, y2);
-			putchar(32);
-			return;
-		}
+		Console::gotoXY(x2 - 1, y2);
+		putchar(32);
+		return;
 	}
 }
 
-void GamePlay::drawL(Cell** pBoard, Cell cell_1, Cell cell_C, Cell cell_2) {
+void Board::drawL(Cell cell_1, Cell cell_C, Cell cell_2) {
 	Console::setColor(WHITE, RED);
-	Cell tmp_1 = cell_1;
-	Cell tmp_2 = cell_2;
-	Cell tmp_C = cell_C;
-	int x1 = (tmp_1.x_console * 10) + 5;
-	int y1 = (tmp_1.y_console * 4) + 2;
-	int x2 = (tmp_2.x_console * 10) + 5;
-	int y2 = (tmp_2.y_console * 4) + 2;
-	int xC = (tmp_C.x_console * 10) + 5;
-	int yC = (tmp_C.y_console * 4) + 2;
-	// Up-right
+	int x1 = (cell_1.x_console * 10) + 5;
+	int y1 = (cell_1.y_console * 4) + 2;
+	int x2 = (cell_2.x_console * 10) + 5;
+	int y2 = (cell_2.y_console * 4) + 2;
+	int xC = (cell_C.x_console * 10) + 5;
+	int yC = (cell_C.y_console * 4) + 2;
+	// Down-right
 	if (xC < x2 && yC > y1) {
 		Console::gotoXY(x1, y1 + 1);
 		putchar(30);
@@ -146,7 +91,7 @@ void GamePlay::drawL(Cell** pBoard, Cell cell_1, Cell cell_C, Cell cell_2) {
 	// Hoán vị cell_1 và cell_2
 	if (xC < x1 && yC > y2) {
 		Console::gotoXY(x1 - 1, y1);
-		putchar(30);
+		putchar(16);
 		for (int i = x1 - 2; i >= xC - 1; i--) {
 			Console::gotoXY(i, y1);
 			putchar(45);
@@ -156,11 +101,11 @@ void GamePlay::drawL(Cell** pBoard, Cell cell_1, Cell cell_C, Cell cell_2) {
 			putchar(179);
 		}
 		Console::gotoXY(x2, y2 - 1);
-		putchar(16);
+		putchar(30);
 		return;
 	}
 	//========================================================================//
-	// Down-right
+	// Up-right
 	if (xC < x2 && yC < y1) {
 		Console::gotoXY(x1, y1 - 1);
 		putchar(31);
@@ -179,21 +124,21 @@ void GamePlay::drawL(Cell** pBoard, Cell cell_1, Cell cell_C, Cell cell_2) {
 	// Hoán vị cell-1 và cell_2
 	if (xC < x1 && yC < y2) {
 		Console::gotoXY(x1 - 1, y1);
-		putchar(31);
+		putchar(16);
 		for (int i = x1 - 2; i >= xC + 1; i--) {
 			Console::gotoXY(i, y1);
 			putchar(45);
 		}
-		for (int i = yC; i <= y2 - 2; i++) {
+		for (int i = yC + 1; i <= y2 - 2; i++) {
 			Console::gotoXY(x2, i);
 			putchar(179);
 		}
 		Console::gotoXY(x2, y2 - 1);
-		putchar(16);
+		putchar(31);
 		return;
 	}
 	//========================================================================//
-	// Down-left
+	// Up-left
 	if (yC < y2 && xC > x1) {
 		Console::gotoXY(x1 + 1, y1);
 		putchar(17);
@@ -212,21 +157,21 @@ void GamePlay::drawL(Cell** pBoard, Cell cell_1, Cell cell_C, Cell cell_2) {
 	//Hoán vị trí cell_1 và cell_2
 	if (yC < y1 && xC > x2) {
 		Console::gotoXY(x1, y1 - 1);
-		putchar(17);
+		putchar(31);
 		for (int i = y1 - 2; i >= yC; i--) {
 			Console::gotoXY(x1, i);
 			putchar(179);
 		}
-		for (int i = xC - 1; i >= x2 + 2; i--) {
+		for (int i = xC; i >= x2 + 2; i--) {
 			Console::gotoXY(i, y2);
 			putchar(45);
 		}
 		Console::gotoXY(x2 + 1, y2);
-		putchar(31);
+		putchar(17);
 		return;
 	}
 	//========================================================================//
-	// Up-left
+	// Down-left
 	if (yC > y2 && xC > x1) {
 		Console::gotoXY(x1 + 1, y1);
 		putchar(17);
@@ -245,7 +190,7 @@ void GamePlay::drawL(Cell** pBoard, Cell cell_1, Cell cell_C, Cell cell_2) {
 	//Hoán vị trí cell_1 và cell_2
 	if (yC > y1 && xC > x2) {
 		Console::gotoXY(x1, y1 + 1);
-		putchar(17);
+		putchar(30);
 		for (int i = y1 + 2; i <= yC; i++) {
 			Console::gotoXY(x1, i);
 			putchar(179);
@@ -255,23 +200,20 @@ void GamePlay::drawL(Cell** pBoard, Cell cell_1, Cell cell_C, Cell cell_2) {
 			putchar(45);
 		}
 		Console::gotoXY(x2 + 1, y2);
-		putchar(30);
+		putchar(16);
 		return;
 	}
 }
 
-void GamePlay::deleteL(Cell** pBoard, Cell cell_1, Cell cell_C, Cell cell_2) {
+void Board::deleteL(Cell cell_1, Cell cell_C, Cell cell_2) {
 	Console::setColor(WHITE, WHITE);
-	Cell tmp_1 = cell_1;
-	Cell tmp_2 = cell_2;
-	Cell tmp_C = cell_C;
-	int x1 = (tmp_1.x_console * 10) + 5;
-	int y1 = (tmp_1.y_console * 4) + 2;
-	int x2 = (tmp_2.x_console * 10) + 5;
-	int y2 = (tmp_2.y_console * 4) + 2;
-	int xC = (tmp_C.x_console * 10) + 5;
-	int yC = (tmp_C.y_console * 4) + 2;
-	// Up-right
+	int x1 = (cell_1.x_console * 10) + 5;
+	int y1 = (cell_1.y_console * 4) + 2;
+	int x2 = (cell_2.x_console * 10) + 5;
+	int y2 = (cell_2.y_console * 4) + 2;
+	int xC = (cell_C.x_console * 10) + 5;
+	int yC = (cell_C.y_console * 4) + 2;
+	// Down-right
 	if (xC < x2 && yC > y1) {
 		Console::gotoXY(x1, y1 + 1);
 		putchar(32);
@@ -303,7 +245,7 @@ void GamePlay::deleteL(Cell** pBoard, Cell cell_1, Cell cell_C, Cell cell_2) {
 		return;
 	}
 	//========================================================================//
-	// Down-right
+	// Up-right
 	if (xC < x2 && yC < y1) {
 		Console::gotoXY(x1, y1 - 1);
 		putchar(32);
@@ -336,7 +278,7 @@ void GamePlay::deleteL(Cell** pBoard, Cell cell_1, Cell cell_C, Cell cell_2) {
 		return;
 	}
 	//========================================================================//
-	// Down-left
+	// Up-left
 	if (yC < y2 && xC > x1) {
 		Console::gotoXY(x1 + 1, y1);
 		putchar(32);
@@ -369,7 +311,7 @@ void GamePlay::deleteL(Cell** pBoard, Cell cell_1, Cell cell_C, Cell cell_2) {
 		return;
 	}
 	//========================================================================//
-	// Up-left
+	// Down-left
 	if (yC > y2 && xC > x1) {
 		Console::gotoXY(x1 + 1, y1);
 		putchar(32);
@@ -403,7 +345,7 @@ void GamePlay::deleteL(Cell** pBoard, Cell cell_1, Cell cell_C, Cell cell_2) {
 	}
 }
 
-void GamePlay::drawZ(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, Cell cell_2) {
+void Board::drawZ(Cell cell_1, Cell cell_C1, Cell cell_C2, Cell cell_2) {
 	Console::setColor(WHITE, RED);
 	int x1 = (cell_1.x_console * 10) + 5;
 	int y1 = (cell_1.y_console * 4) + 2;
@@ -415,7 +357,7 @@ void GamePlay::drawZ(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, Cel
 	int yC2 = (cell_C2.y_console * 4) + 2;
 	// Vẽ Z cũng có 4 dạng 
 
-   // Dạng 1: Right-Down-Right
+   // Dạng 1: Right-Up-Right
 	if (xC1 > x1 && yC1 > y2) {
 		Console::gotoXY(x1 + 1, y1);
 		putchar(17);
@@ -438,24 +380,24 @@ void GamePlay::drawZ(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, Cel
 	// Đi ngược lại
 	if (xC1 < x1 && yC1 < y2) {
 		Console::gotoXY(x1 - 1, y1);
-		putchar(17);
+		putchar(16);
 		for (int i = x1 - 2; i >= xC1; i--) {
 			Console::gotoXY(i, y1);
 			putchar(45);
 		}
-		for (int i = yC1 + 1; i <= yC2 - 1; i++) {
+		for (int i = yC1 + 1; i <= yC2; i++) {
 			Console::gotoXY(xC1, i);
 			putchar(179);
 		}
-		for (int i = xC2; i >= x2 - 2; i--) {
+		for (int i = xC2; i >= x2 + 2; i--) {
 			Console::gotoXY(i, yC2);
 			putchar(45);
 		}
-		Console::gotoXY(x2 - 1, y2);
-		putchar(16);
+		Console::gotoXY(x2 + 1, y2);
+		putchar(17);
 		return;
 	}
-	// Dạng 2: Right-Up-Right
+	// Dạng 2: Right-Down-Right
 	if (xC1 > x1 && yC1 < y2) {
 		Console::gotoXY(x1 + 1, y1);
 		putchar(17);
@@ -478,7 +420,7 @@ void GamePlay::drawZ(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, Cel
 	// Đi ngược lại
 	if (xC1 < x1 && yC1 > y2) {
 		Console::gotoXY(x1 - 1, y1);
-		putchar(17);
+		putchar(16);
 		for (int i = x1 - 2; i >= xC1; i--) {
 			Console::gotoXY(i, y1);
 			putchar(45);
@@ -487,16 +429,16 @@ void GamePlay::drawZ(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, Cel
 			Console::gotoXY(xC1, i);
 			putchar(179);
 		}
-		for (int i = xC2; i >= x2 - 2; i--) {
+		for (int i = xC2; i >= x2 + 2; i--) {
 			Console::gotoXY(i, yC2);
 			putchar(45);
 		}
-		Console::gotoXY(x2 - 1, y2);
-		putchar(16);
+		Console::gotoXY(x2 + 1, y2);
+		putchar(17);
 		return;
 	}
 
-	// Dạng 3: Up-right-up
+	// Dạng 3: Down-right-down
 	if (xC1 < x2 && yC1 > y1) {
 		Console::gotoXY(x1, y1 + 1);
 		putchar(30);
@@ -519,25 +461,25 @@ void GamePlay::drawZ(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, Cel
 	// Đi ngược lại
 	if (xC1 > x2 && yC1 < y1) {
 		Console::gotoXY(x1, y1 - 1);
-		putchar(30);
+		putchar(31);
 		for (int i = y1 - 2; i >= yC1 + 1; i--) {
 			Console::gotoXY(x1, i);
 			putchar(179);
 		}
-		for (int i = xC1; i >= xC2 - 1; i--) {
+		for (int i = xC1; i >= xC2; i--) {
 			Console::gotoXY(i, yC1);
 			putchar(45);
 		}
-		for (int i = yC2; i >= y2 - 2; i--) {
+		for (int i = yC2; i >= y2 + 2; i--) {
 			Console::gotoXY(x2, i);
 			putchar(179);
 		}
-		Console::gotoXY(x2, y2 - 1);
-		putchar(31);
+		Console::gotoXY(x2, y2 + 1);
+		putchar(30);
 		return;
 	}
 
-	// Dạng 4: Down-right-down
+	// Dạng 4: Up-right-Up
 	if (xC1 > x2 && yC1 > y1) {
 		Console::gotoXY(x1, y1 + 1);
 		putchar(30);
@@ -560,7 +502,7 @@ void GamePlay::drawZ(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, Cel
 	//Đi ngược lại
 	if (xC1 < x2 && yC1 < y1) {
 		Console::gotoXY(x1, y1 - 1);
-		putchar(30);
+		putchar(31);
 		for (int i = y1 - 2; i >= yC1 + 1; i--) {
 			Console::gotoXY(x1, i);
 			putchar(179);
@@ -569,17 +511,17 @@ void GamePlay::drawZ(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, Cel
 			Console::gotoXY(i, yC1);
 			putchar(45);
 		}
-		for (int i = yC2 - 1; i >= y2 - 2; i--) {
+		for (int i = yC2 - 1; i >= y2 + 2; i--) {
 			Console::gotoXY(x2, i);
 			putchar(179);
 		}
-		Console::gotoXY(x2, y2 - 1);
-		putchar(31);
+		Console::gotoXY(x2, y2 + 1);
+		putchar(30);
 		return;
 	}
 }
 
-void GamePlay::deleteZ(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, Cell cell_2) {
+void Board::deleteZ(Cell cell_1, Cell cell_C1, Cell cell_C2, Cell cell_2) {
 	Console::setColor(WHITE, WHITE);
 	int x1 = (cell_1.x_console * 10) + 5;
 	int y1 = (cell_1.y_console * 4) + 2;
@@ -754,7 +696,7 @@ void GamePlay::deleteZ(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, C
 	}
 }
 
-void GamePlay::drawU(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, Cell cell_2) {
+void Board::drawU(Cell cell_1, Cell cell_C1, Cell cell_C2, Cell cell_2) {
 	Console::setColor(WHITE, RED);
 	int x1 = (cell_1.x_console * 10) + 5;
 	int y1 = (cell_1.y_console * 4) + 2;
@@ -927,7 +869,7 @@ void GamePlay::drawU(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, Cel
 	}
 }
 
-void GamePlay::deleteU(Cell** pBoard, Cell cell_1, Cell cell_C1, Cell cell_C2, Cell cell_2) {
+void Board::deleteU(Cell cell_1, Cell cell_C1, Cell cell_C2, Cell cell_2) {
 	Console::setColor(WHITE, WHITE);
 	int x1 = (cell_1.x_console * 10) + 5;
 	int y1 = (cell_1.y_console * 4) + 2;
